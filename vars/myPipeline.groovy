590c8f1dca4ef821a2019a82eaee2df10d65ecc1
Marton Szabo <szbmarton@gmail.com>
2019-11-22 16:46:48 +0100

    add vars

---
+++ vars/myPipeline.groovy
@@ -0,0 +1,36 @@
+def call(Map pipelineParams) {
+
+    pipeline {
+        agent any
+        stages {
+            stage('checkout git') {
+                steps {
+                    echo "git checkout: ${pipelineParams.branch} ${pipelineParams.url}"
+                }
+            }
+
+            stage('build') {
+                steps {
+                    echo "mvn clean package -DskipTests=true"
+                }
+            }
+
+            stage ('test') {
+                steps {
+                    echo "run test"
+                }
+            }
+			
+            stage ('deploy') {
+                steps {
+                    parallel (
+                        "deploy dev": { echo "deploy test env: ${pipelineParams.developmentServer}" },
+                        "deploy staging": { echo "deploy test env: ${pipelineParams.stagingServer}" }
+                    )
+                }
+            }
+
+        }
+
+    }
+}
